bdb669771231b2af69b0303bd847aec5b5d8735d
NemethBalint96 <nemeth.balint1996@gmail.com>
2023-09-23 18:43:12 +0200

    Custom income creation

--- app/(root)/(rootes)/income/components/client.tsx
+++ app/(root)/(rootes)/income/components/client.tsx
@@ -4,6 +4,9 @@ import { Heading } from "@/components/ui/heading"
 import { TodayIncomeColumn, TransactionColumn, incomeColumns, todayIncomeColumns } from "./columns"
 import { DataTable } from "@/components/ui/data-table"
 import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button"
+import { Plus } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 interface IncomeClientProps {
   todayData: TodayIncomeColumn[]
@@ -11,6 +14,8 @@ interface IncomeClientProps {
 }
 
 const IncomeClient: React.FC<IncomeClientProps> = ({ todayData, data }) => {
+  const router = useRouter()
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -18,6 +23,10 @@ const IncomeClient: React.FC<IncomeClientProps> = ({ todayData, data }) => {
           title="Income"
           description="Today's income"
         />
+        <Button onClick={() => router.push("/income/new")}>
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
       </div>
       <DataTable
         columns={todayIncomeColumns}
---
+++ app/(root)/(rootes)/income/new/page.tsx
@@ -0,0 +1,13 @@
+import { IncomeForm } from "./components/income-form"
+
+const ExpensePage = async () => {
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <IncomeForm />
+      </div>
+    </div>
+  )
+}
+
+export default ExpensePage
